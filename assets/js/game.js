6bf57876cbdd18de728452b3e9a12d01fb219201
vcaruso <vinnycar0923@gmail.com>
2020-06-03 17:06:48 -0700

    MVP Finished!

--- assets/js/game.js
+++ assets/js/game.js
@@ -60,15 +60,39 @@ var fight = function(enemyName) {
     }
 };
 
-for(var i = 0; i < enemyNames.length; i++) {
-    if (playerHealth > 0) {
-        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-        var pickedEnemyName = enemyNames[i];
-        enemyHealth = 50;
-        fight(pickedEnemyName);
-    }
-    else {
-        window.alert("You have lost your robot in battle! Game Over!");
-        break;
-    }
-  }
+var startGame = function () {
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+    for(var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+            var pickedEnemyName = enemyNames[i];
+            enemyHealth = 50;
+            fight(pickedEnemyName);
+        }
+        else {
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+        };
+    };
+    endGame();
+};
+
+var endGame = function () {
+    if (playerHealth >= 0) {
+        window.alert("Great job, you survived the robot onslaught! You know have a score");
+    } else {
+        window.alert("You've lost your robot in battle :(.");
+    };
+
+    var playAgainConfirm = window.confirm("Would you like to play again?")
+        if (playAgainConfirm) {
+            startGame();
+        } else {
+            window.alert("Thank you for playing Robot Gladiators! See you next time!")
+        }
+}
+
+startGame();
+
