15491f754e5b9ffb05210d85bcf5d1791a5779b8
vcaruso <vinnycar0923@gmail.com>
2020-06-05 15:38:25 -0700

    Choose store options with number

--- assets/js/game.js
+++ assets/js/game.js
@@ -93,25 +93,21 @@ var endGame = function () {
 
 var shop = function() {
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter '1' to REFILL, '2' to UPGRADE, or '3' to LEAVE."
     );
+        shopOptionPrompt = parseInt(shopOptionPrompt);
 
     switch (shopOptionPrompt) {
-        case "refill":
-        case "REFILL":
-        case "Refill":
+        case 1:
             playerInfo.refillHealth();
             break;
 
-        case "upgrade":
-        case "UPGRADE":
-        case "Upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
 
-        case "leave":
-        case "LEAVE":
-        case "Leave":
+        case 3:
+            window.alert("Leaving the shop!")
             break;
 
         default:
